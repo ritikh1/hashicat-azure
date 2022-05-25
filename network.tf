d96451953a2f570a52c7c8ad992322250fa5ca13
Ritikh Saxnea <saxena.ritikh@outlook.com>
2022-05-25 14:28:20 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/ritikh-tf/network/azurerm"
   version = "3.5.0"
-  resource "azurerm_resource_group" "myresourcegroup"
+  resource_group_name "azurerm_resource_group" "myresourcegroup"
   
 }
